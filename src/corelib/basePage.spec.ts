7809e53dac3bcd70fd07ee50b428af8b3659bc77
@Atemkengfranck2 <atemkengfalcao2@gmail.com>
2025-01-29 23:17:43 +0100

    configuration et gérer le contexte du navigateur

---
+++ src/corelib/basePage.spec.ts
@@ -0,0 +1,24 @@
+import { setDefaultTimeout,Before, After } from '@cucumber/cucumber';
+import { Browser, BrowserContext, Page, chromium } from 'playwright';
+import { expect } from '@playwright/test';
+
+setDefaultTimeout(12000);
+
+let browser: Browser;
+let bCtx: BrowserContext;
+let page: Page;
+
+Before(async function () {
+  browser = await chromium.launch({ headless: false, channel: "chrome", args: ['--start-maximized'] });
+  bCtx= await browser.newContext({viewport: null, javaScriptEnabled:true});
+  page= await bCtx.newPage();
+});
+
+//After Hook
+After(async function () {
+  await page.close();
+  await bCtx.close();
+  await browser.close();
+});
+
+export { page, browser, bCtx };
