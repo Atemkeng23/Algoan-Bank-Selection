599396495ea3eacecf36296581949ef0495b7e58
@Atemkengfranck2 <atemkengfalcao2@gmail.com>
2025-01-29 23:20:36 +0100

    définition les étapes de test spécifiques

---
+++ src/test/pages/BankSelectionPage.ts
@@ -0,0 +1,34 @@
+import { Page } from '@playwright/test';
+
+export class BankSelectionPage {
+    readonly page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    async navigateTo() {
+        await this.page.goto('https://connect.preprod.algoan.com/v2/bank-selection?client_id=f7be049b0df73459d476fb2d&redirect_uri=https://dashboard.preprod.algoan.com');
+    }
+
+    async checkTermsAndConditions() {
+        await this.page.getByRole('checkbox').check();
+    }
+
+    async clickContinueButton() {
+        await this.page.getByRole('button', { name: 'Continuer vers le choix de la' }).click();
+    }
+
+    async selectBank(bankName: string) {
+        await this.page.getByText('Algoan Bank').click();
+    }
+
+    async clickJaiComprisButton() {
+        await this.page.getByTestId('bank-redirection-button').click();
+    }
+
+    async verifyRedirection() {
+        await this.page.waitForSelector('text=Bienvenue sur Algoan Bank', { timeout: 120 * 1000 });
+    }
+
+}
